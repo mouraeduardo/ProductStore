931fc383a1410e51c8e36be1edcecb15d25a924f
mouraeduardo <eduardomoura2001@hotmail.com>
2023-05-08 21:53:31 -0300

    criação do mapeamento de resource para models, e mudanças no appsettings para edição no cors

---
+++ Product.Store/Util/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ProductStore.Domain.Models;
+using ProductStore.Resources;
+
+namespace ProductStore.Util.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<User, UserResource>();
+        }
+    }
+}
